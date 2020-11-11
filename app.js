d583f269ec6cc60a337e8dae9fb2c01e85e48c04
mikelbrierly <mikelbrierly@protonmail.com>
2020-11-11 15:10:17 -0600

    done with initial version

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
-const littlePicture = document.querySelector('.small-picture');
-const url = document.querySelector('#urlInput');
-const button = document.querySelector('.update-button');
+const littlePicture = document.querySelector(".small-picture");
+const userForm = document.querySelector(".update-url");
+const url = document.querySelector(".update-url input");
+const updateUrlButton = document.querySelector(".update-url button");
+const toggleFormButton = document.querySelector("footer button");
 
-url.addEventListener('keyup', function(event) {
-  if(event.keyCode === 13) {
+url.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
     updateUrl(url.value);
   }
-})
+});
 
-button.addEventListener('click', function(event) {
+toggleFormButton.addEventListener("click", function(event) {
+  resetForm();
+});
+
+updateUrlButton.addEventListener("click", function(event) {
   updateUrl(url.value);
-})
+});
 
 const updateUrl = function(newUrl) {
-  console.log(newUrl);
   littlePicture.src = newUrl;
-}
+  resetForm();
+};
+
+const resetForm = function() {
+  if (userForm.classList.contains("hidden")) {
+    userForm.classList.remove("hidden");
+    toggleFormButton.innerHTML = "cancel";
+    return;
+  }
+  userForm.classList.add("hidden");
+  toggleFormButton.innerHTML = "change it up again";
+};
